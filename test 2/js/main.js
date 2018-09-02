824a2391865f361c314104a8dc7cc8235abfe7e0
nico-pwmt <nico.pwmt@gmail.com>
2018-09-02 21:36:49 +0200

    2/9
    related links

--- test 2/js/main.js
+++ test 2/js/main.js
@@ -1,38 +1,12 @@
-/*
-document.querySelector(".add").addEventListener("click", function() {
-	var number = document.querySelector(".number").value;
-	var result = document.querySelector(".result ol").innerHTML;
-	document.querySelector(".result ol").innerHTML = Number(number) + Number(result);
-	document.querySelector(".number").value = "";
-});
-*/
-
-/*
-document.querySelector(".add").addEventListener("click", function() {
-	var number = document.querySelector(".number").value;
-	var list = document.querySelector(".result ol");
-	var li = document.createElement("LI");
-	var txtNode = document.createTextNode(number);
-	li.appendChild(txtNode);
-	list.appendChild(li);
-	document.querySelector(".number").value = "";
-	var total = document.querySelector(".total");
-	total.innerHTML = Number(total.innerHTML) + Number(li.innerHTML);
-});
-*/
-
 var summary = [];
 
 var funcSuma = function() {
 	var sum = 0;
 	var total = document.querySelector(".total");
-	for (i = 0; i < summary.length; i++) {
+	for (var i = 0; i < summary.length; i++) {
 		sum = sum + Number(summary[i].itemQuantity);
 	}
 	total.innerHTML = sum;
-	if (summary.length == 0) {
-		total.innerHTML = 0;
-	}
 }
 
 
@@ -70,5 +44,85 @@ document.querySelector(".emptyCarr").addEventListener("click", function() {
 	summary = [];
 	var result = document.querySelector(".result dl");
 	result.innerHTML = "";
+	funcSuma();
+});
+
+document.querySelector(".add2medias").addEventListener("click", function() {
+	var product = "Medias";
+	var number = 2;
+	var item = {
+		productName: product,
+		itemQuantity: number
+	}
+	summary.push(item);
+	var result = document.querySelector(".result dl");
+	var dt = document.createElement("DT");
+	var dd = document.createElement("DD");
+	var dtTextNode = document.createTextNode(product);
+	var ddTextNode = document.createTextNode(number);
+	dt.appendChild(dtTextNode);
+	dd.appendChild(ddTextNode);
+	result.appendChild(dt);
+	result.appendChild(dd);
+	funcSuma();
+});
+
+document.querySelector(".add1htc").addEventListener("click", function() {
+	var product = "Htc";
+	var number = 1;
+	var item = {
+		productName: product,
+		itemQuantity: number
+	}
+	summary.push(item);
+	var result = document.querySelector(".result dl");
+	var dt = document.createElement("DT");
+	var dd = document.createElement("DD");
+	var dtTextNode = document.createTextNode(product);
+	var ddTextNode = document.createTextNode(number);
+	dt.appendChild(dtTextNode);
+	dd.appendChild(ddTextNode);
+	result.appendChild(dt);
+	result.appendChild(dd);
+	funcSuma();
+});
+
+document.querySelector(".add5bollis").addEventListener("click", function() {
+	var product = "Bollis";
+	var number = 5;
+	var item = {
+		productName: product,
+		itemQuantity: number
+	}
+	summary.push(item);
+	var result = document.querySelector(".result dl");
+	var dt = document.createElement("DT");
+	var dd = document.createElement("DD");
+	var dtTextNode = document.createTextNode(product);
+	var ddTextNode = document.createTextNode(number);
+	dt.appendChild(dtTextNode);
+	dd.appendChild(ddTextNode);
+	result.appendChild(dt);
+	result.appendChild(dd);
+	funcSuma();
+});
+
+document.querySelector(".add9muslims").addEventListener("click", function() {
+	var product = "Muslims";
+	var number = 9;
+	var item = {
+		productName: product,
+		itemQuantity: number
+	}
+	summary.push(item);
+	var result = document.querySelector(".result dl");
+	var dt = document.createElement("DT");
+	var dd = document.createElement("DD");
+	var dtTextNode = document.createTextNode(product);
+	var ddTextNode = document.createTextNode(number);
+	dt.appendChild(dtTextNode);
+	dd.appendChild(ddTextNode);
+	result.appendChild(dt);
+	result.appendChild(dd);
 	funcSuma();
 });
