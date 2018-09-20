a80a60c743a22d2004d888f88471af37df424d85
nico-pwmt <nico.pwmt@gmail.com>
2018-09-20 22:34:56 +0200

    flat arrays and chessboard location

--- test 2/js/main.js
+++ test 2/js/main.js
@@ -9,7 +9,7 @@
 			sum = sum + Number(summary[i].itemQuantity);
 		}
 		total.innerHTML = sum;
-	}
+	};
 
 	document.querySelector(".add").addEventListener("click", function() {
 		var product = document.querySelector(".product").value;
@@ -68,6 +68,8 @@
 		funcSuma();
 	});
 
+
+
 	/* TOOLTIP */
 
 	var $tooltip;
@@ -106,6 +108,8 @@
 		$tooltip.remove();
 	});
 
+
+
 	/* LIGHTBOXES */
 
 	$(".openLightbox").on("click", function() {
@@ -132,122 +136,147 @@
 		$(".holderFlex").hide();
 	})
 
+
+
 	/* AUTOCOMPLETE */
 
 	var countries = ["Argentina", "dogistan", "sarasaland", "zaragoza"];
 
-	function autocomplete(inp, arr) {
-  /*the autocomplete function takes two arguments,
-  the text field element and an array of possible autocompleted values:*/
-  var currentFocus;
-  /*execute a function when someone writes in the text field:*/
-  inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
-      /*close any already open lists of autocompleted values*/
-      closeAllLists();
-      if (!val) { return false;}
-      currentFocus = -1;
-      /*create a DIV element that will contain the items (values):*/
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
-      this.parentNode.appendChild(a);
-      /*for each item in the array...*/
-      for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
-              b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
-              inp.value = this.getElementsByTagName("input")[0].value;
-              /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
-              closeAllLists();
-          });
-          a.appendChild(b);
-        }
-      }
-  });
-  /*execute a function presses a key on the keyboard:*/
-  inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
-      if (x) x = x.getElementsByTagName("div");
-      if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
-        currentFocus++;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
-        currentFocus--;
-        /*and and make the current item more visible:*/
-        addActive(x);
-      } else if (e.keyCode == 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
-        e.preventDefault();
-        if (currentFocus > -1) {
-          /*and simulate a click on the "active" item:*/
-          if (x) x[currentFocus].click();
-        }
-      }
-  });
-  function addActive(x) {
-    /*a function to classify an item as "active":*/
-    if (!x) return false;
-    /*start by removing the "active" class on all items:*/
-    removeActive(x);
-    if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = (x.length - 1);
-    /*add class "autocomplete-active":*/
-    x[currentFocus].classList.add("autocomplete-active");
-  }
-  function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
-    for (var i = 0; i < x.length; i++) {
-      x[i].classList.remove("autocomplete-active");
-    }
-  }
-  function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
-    var x = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < x.length; i++) {
-      if (elmnt != x[i] && elmnt != inp) {
-      x[i].parentNode.removeChild(x[i]);
-    }
-  }
-}
-/*execute a function when someone clicks in the document:*/
-document.addEventListener("click", function (e) {
-    closeAllLists(e.target);
-});
-}
+	var suggestions = $(".js-suggest");
 
-autocomplete(document.getElementById("myInput"), countries);
+	suggestions.one("keypress", function() {
+		var $list = $("<ul class='suggestions'></ul>");
+		var position = $(this).offset();
+		var width = $(this).outerWidth();
+		var height = $(this).outerHeight();
+
+		$("body").append($list);
+		for (var i = 0; i < countries.length; i++) {
+			$list.append("<li>" + countries[i] + "</li>");
+		};
+		$list.css({left: position.left, top: position.top + height, width: width});
+		var item = $(".suggestions li");
+		item.on("click", function() {
+			var itemValue = item.html();
+			suggestions.val(itemValue);
+		});
+	});
+
+	var suggestions2 = $(".js-suggest2");
+
+	suggestions2.on("click", function() {
+		$.ajax({
+			type: "GET",
+			url: "endpoint.json",
+			data: { get_param: 'value' },
+			dataType: "json", 
+			success: function(data) {
+				console.log(data);
+		}});
+	});
+
+	/*
+	suggestions2.on("click", function() {
+		$.getJSON("endpoint.json", function(result){
+			console.log(result);
+		});
+	});
+	*/
+
+
+
+	/* FLAT ARRAY */
+
+	var multidimensional = [1, 2, 4, "dog", true, [12, "chabon", ["5", 67], 53], "banana"];
+	$(".multidimensionalArray").on("click", function() {
+		console.log(multidimensional);
+	});
+	/*
+	--- VERSION SOLO BI-DIMENSIONAL, WORTH IT? ---
+	$(".flattenArray").on("click", function() {
+		console.log(multidimensional.flat());
+	});
+	*/
+	
+	function flatten(arr) {
+		return arr.reduce((accumulator, currentValue) =>  Array.isArray(currentValue) ? accumulator.concat(flatten(currentValue)) : accumulator.concat(currentValue), []);
+	}
+	
+	/*
+	// --- TRATE DE PASARLO A ES5, Q ESTA MAL? (el "?" y ":" falla al pasarlo a if else idk why) ---
+	function flatten(arr) {
+		return arr.reduce(function (accumulator, currentValue) {
+			if (Array.isArray(currentValue)) {
+				return accumulator.concat(flatten(currentValue));
+			} else {
+				accumulator.concat(currentValue)
+			}
+		}, []);
+	}
+	*/
+
+	$(".flattenArray").on("click", function() {
+		console.log(flatten(multidimensional));
+	});
+
+
+
+	/* CHESS BOARD */
+
+	var message = $(".chessBoard .message");
+	$(".chessBoard td").on("click", function() {
+		message.html("The position is " + Math.floor($(this).offset().left) + " X and " + Math.floor($(this).offset().top) + " Y");
+		setTimeout(function() {
+			message.html("")
+		}, 1000)
+	});
 
 })();
 
 
 
 
-/* 
 
-	autocomplete 
+
+
+
+
+
+/*
+
+	autocomplete
 	solo sugiere a partir de 3 caracteres
 	consulta ajax jquery --
-	hacer un array 
+	hacer un array
 	cualquier input con clase js-suggest
 
+
+
+
+	+3 caracteres pasa algo
+	-3 otra cosa
+
+	mandar info a endpoint
+	mandar string con ajax
+
+	devuelve endpoint qe es el objeto
+
+	valores del objeto debajo del input
+
+
+
+
+
+
+
+
+
+
+
+
+	cookies vs localstorage vs sessionstorage
+	consultas http tipos get post
+	objetos
 
 */
 
