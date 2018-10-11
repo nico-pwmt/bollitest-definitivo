c1d4c08234e180f2ce5ea5ea222b35e660ed3809
nico-pwmt <nico.pwmt@gmail.com>
2018-10-11 16:25:20 +0200

    mucho

--- test 2/js/main.js
+++ test 2/js/main.js
@@ -1,5 +1,7 @@
 (function() {
 
+	var NUMBER_PI = 3.14;
+
 	var summary = [];
 
 	var funcSuma = function() {
@@ -123,23 +125,29 @@
 	});
 
 	$(".openVertLightbox").on("click", function() {
-		$(".holderVert").show();
+		$(".holderVert").removeClass("is-hidden");
 	});
-	$(".vertLightbox .close").on("click", function() {
-		$(".holderVert").hide();
+	$(".vertLightbox .close, .holderVert").on("click", function() {
+		$(".holderVert").addClass("is-hidden");
+	});
+	$(".vertLightbox").on("click", function(event) {
+		event.stopPropagation();
 	});
 
 	$(".openFlexLightbox").on("click", function() {
-		$(".holderFlex").css("display", "flex");
+		$(".holderFlex").removeClass("is-hidden");
 	});
-	$(".flexLightbox .close").on("click", function() {
-		$(".holderFlex").hide();
+	$(".flexLightbox .close, .holderFlex").on("click", function() {
+		$(".holderFlex").addClass("is-hidden");
 	})
+	$(".flexLightbox").on("click", function(event) {
+		event.stopPropagation();
+	});
 
 
 
 	/* AUTOCOMPLETE */
-
+/*
 	var countries = ["Argentina", "dogistan", "sarasaland", "zaragoza"];
 
 	var suggestions = $(".js-suggest");
@@ -182,126 +190,164 @@
 		});
 	});
 	*/
+	
+	var suggestions = $(".js-suggest");
+	suggestions.on("keyup", function() {
+		suggestions.toggleClass("valid", $(this).val().length >= 3);
+		if ($(this).val().length >= 3) {
+			$.ajax({
+				url: "https://jsonplaceholder.typicode.com/posts",
+				success: function(response) {
+					for (var i = 0; i < 5; i++) {
+						console.log(response[i].title);
+					}
+				},
 
+			});
+		}
+
+	});
 
 
 	/* FLAT ARRAY */
 
-	var multidimensional = [1, 2, 4, "dog", true, [12, "chabon", ["5", 67], 53], "banana"];
+	var multidimensional = [1, 2, 4, "dog", true, [12, "chabon", ["deep", "shit",["even deeper"], 53]], "banana"];
+
 	$(".multidimensionalArray").on("click", function() {
 		console.log(multidimensional);
 	});
 
-
-
-	/*
-	// --- VERSION SOLO BI-DIMENSIONAL, WORTH IT? ---
-	$(".flattenArray").on("click", function() {
-		console.log(multidimensional.flat());
-	});
-	*/
-	/*
-	// ------- EL POSTA -------
-	function flatten(arr) {
-		return arr.reduce((accumulator, currentValue) =>  Array.isArray(currentValue) ? accumulator.concat(flatten(currentValue)) : accumulator.concat(currentValue), []);
-	}
-	*/
-	/*
-	// --- TRATE DE PASARLO A ES5, Q ESTA MAL? (el "?" y ":" falla al pasarlo a if else idk why) ---
-	function flatten(arr) {
-		return arr.reduce(function (accumulator, currentValue) {
-			if (Array.isArray(currentValue)) {
-				return accumulator.concat(flatten(currentValue));
+	function flattenArray(array) {
+		var flattened = [];
+		for (var i = 0; i < array.length; i++) {
+			if (!Array.isArray(array[i])) {
+				flattened.push(array[i]);
 			} else {
-				accumulator.concat(currentValue)
-			}
-		}, []);
-	}
-	*/
-	
-	// -------- MI INTENTO --------
-	
-	function flatten(arr) {
-		for (var i = 0; i < multidimensional.length; i++) {
-			if (Array.isArray(multidimensional[i])) {
-				[i].flat()
-			} else {
-
+				flattened = flattened.concat(flattenArray(array[i]));
 			}
 		}
+		return flattened;
 	};
-	
 
 	$(".flattenArray").on("click", function() {
-		console.log(flatten(multidimensional));
+		console.log(flattenArray(multidimensional));
 	});
 
 
 
 	/* CHESS BOARD */
 
-	var message = $(".chessboard .message");
+	var messageTimer;
+	var $message = $(".chessboard .message");
+
+	var $pieceSelector = $(".pieceSelector");
+	$pieceSelector.on("change", function() {
+		console.log($(this).val())
+	});	
+
+	var horsePositionX = Math.floor(Math.random() * 8);
+	var horsePositionY = Math.floor(Math.random() * 8);
+	var $horsePositionCurrent = $(".chessboard .chessboard-row:eq(" + horsePositionY + ") :eq(" + horsePositionX + ")").addClass("horsie");
+	var labelsX = {
+		0: "a",
+		1: "b",
+		2: "c",
+		3: "d",
+		4: "e",
+		5: "f",
+		6: "g",
+		7: "h",
+	};
+	var labelsY = {
+		0: 8,
+		1: 7,
+		2: 6,
+		3: 5,
+		4: 4,
+		5: 3,
+		6: 2,
+		7: 1,
+	};
+
 	$(".chessboard .square").on("click", function() {
-		var posX = $(this).index();
-		var posY = $(this).parent().index();
-		switch (posX) {
-			case 0:
-				posX = "a";
-				break;
-			case 1:
-				posX = "b";
-				break;
-			case 2:
-				posX = "c";
-				break;
-			case 3:
-				posX = "d";
-				break;
-			case 4:
-				posX = "e";
-				break;
-			case 5:
-				posX = "f";
-				break;
-			case 6:
-				posX = "g";
-				break;
-			case 7:
-				posX = "h";
-				break;
+		var clickPositionX = $(this).index();
+		var clickPositionY = $(this).parent().index();
+		var deltaX = Math.abs(horsePositionX - clickPositionX);
+		var deltaY = Math.abs(horsePositionY - clickPositionY);
+		var message = "";
+		if (deltaX == 2 && deltaY == 1 || deltaX == 1 && deltaY == 2) {
+			$horsePositionCurrent.removeClass("horsie");
+			$horsePositionCurrent = $(".chessboard .chessboard-row:eq(" + clickPositionY + ") .square:eq(" + clickPositionX + ")");
+			horsePositionX = clickPositionX;
+			horsePositionY = clickPositionY;
+			$horsePositionCurrent.addClass("horsie");
+		} else if (clickPositionX == horsePositionX && clickPositionY == horsePositionY) {
+			message = "dis is horse";
+		} else {
+			message = "ILLEGAL MOVE";
 		}
-		switch (posY) {
-			case 0:
-				posY = 8;
-				break;
-			case 1:
-				posY = 7;
-				break;
-			case 2:
-				posY = 6;
-				break;
-			case 3:
-				posY = 5;
-				break;
-			case 4:
-				posY = 4;
-				break;
-			case 5:
-				posY = 3;
-				break;
-			case 6:
-				posY = 2;
-				break;
-			case 7:
-				posY = 1;
-				break;
-		}
-		message.html("The position is " + posX + posY);
-		setTimeout(function() {
-			message.html("");
-		}, 1000)
+
+		var messagePosition = "The position is " + labelsX[clickPositionX] + labelsY[clickPositionY];
+		$message.html(messagePosition + "<br>" + message);
+
+		clearTimeout(messageTimer);
+		messageTimer = setTimeout(function() {
+			$message.html("");
+		}, 1000);
+
+
 	});
-	
+
+	/* LOG 1 to 10 */
+
+	$(".log").on("click", function() {
+		for (var i = 1; i <= 10; i++) {
+			(function(i) {
+				setTimeout(function() {
+					console.log(i)
+				}, 1000 + i*100);
+			})(i);
+		}
+	});
+
+	/* FIBONACCI */
+
+	/* NON RECURSIVE */
+	/*
+	$(".fibonacci").on("click", function() {
+		var a = 0;
+		var b = 1;
+		var result = 0;
+		for (var i = 1; i <= 100; i++) {
+			result = a + b;
+			a = b;
+			b = result;
+			console.log(result);
+		}
+	});
+	*/
+	/* RECURSIVE */
+
+	var fibNumber = $(".fibNumber");
+	fibNumber.val(0);
+
+	function fibi(posN) {
+		if (posN == 0) {
+			return 0;
+		} else if (posN == 1) {
+			return 1;
+		} else if (posN < 0 || posN > 100) {
+			return "DEJATE DE JODER";
+		}
+		return fibi(posN - 1) + fibi(posN - 2);
+	};
+
+	$(".fibLogger").on("click", function() {
+		console.log(fibi(fibNumber.val()));
+		fibNumber.val(0);
+	});
+
+
 })();
 
 
@@ -314,55 +360,31 @@
 
 
 
+
+
 /*
 
+	--------
 
-	LIGHTBOX con clases. asi es?
+	un select del chess para elejir piezas
+	ELEJIR SPAWN CHESS PIECES
+	spawn una pieza en random spot y q funcione
+
+	------------
+
+	enviar los titulos abajo del input con un div con 5 a=href
+
+	click afuera del input, desaparece el div
+	focus campo, se muestra, si hay 3 caracteres, unfocus, desaparece
 	
+	q js lo cree al final del body
 
-*/
+	al clickear link, prevent default, y el titulo q clickeo pasa al valor del input
 
+	--------------
 
-
-/*
-
-	AUTOCOMPLETE
-
-SUGERENCIAS:
-- el tooltip de las sugerencias aparecen instantaneo en vez de a los 3 caracteres
-- no se oculta al clickear afuera
-- no se oculta al clickear un item
-- es transparente y tapa todo
-
-	solo sugiere a partir de 3 caracteres
-	consulta ajax jquery --
-	hacer un array
-	cualquier input con clase js-suggest
-
-	+3 caracteres pasa algo
-	-3 otra cosa
-
-	mandar info a endpoint
-	mandar string con ajax
-
-	devuelve endpoint qe es el objeto
-
-	valores del objeto debajo del input
-
-ARRAY FLAT:
-- estoy 100% seguro q copypasteaste codigo, si bien hay funciones de array que aplanan el array por vos, la idea es hacerlo vos mismo algoritmicamente (iterando por el array y agregnado/sacando elementos para armar uno plano) fijate si te sale
-- es jodido el ejercicio, trata de primero pensarlo en papel y dejar la parte de programacion de lado, fijate como lo harias bidimensionalmente primero paso por paso y dsp lo haces multidimensional para agregar dificultad
-
-estas usando flat() de nuevo, la idea es aplanar el array "a mano", solo iterando (for) y agregando/sacando elementos (push/pop). intenta primero con un array de 2 dimensiones nomas y pensa como lo harias paso por paso y dsp traducilo a codigo
+	ta te ti
 
 
-
-
-
-TABLERO:
-- el mensaje lo ocultas con un timeout de un segundo. eso funciona siempre y cuando no hagas dos o muchos clicks seguidos, entendes por que? trata de arreglar eso. idealmente, que se oculte despues de 1 segundo de no clickear otra celda
-
--  lightboxes sin holder como padre, se compli
-no tengo idea que significa esta pregunta :)
 
 */
